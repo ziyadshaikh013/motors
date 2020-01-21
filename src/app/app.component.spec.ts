f8a1505b474a381b2dad69644ee14128bd89896a
ziyadshaikh013 <ziyadshaikh013@gmail.com>
2020-01-21 20:52:56 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'garage'`, () => {
+  it(`should have as title 'farooquemotors'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('garage');
+    expect(app.title).toEqual('farooquemotors');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('garage app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('farooquemotors app is running!');
   });
 });
